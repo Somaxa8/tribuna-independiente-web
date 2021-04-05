9666f7e631fcc26b742bac834ca838f0943f1f65
Somacode <silviofrancoxa8@gmail.com>
2021-04-04 19:03:13 -0500

    added SliderComponent

---
+++ src/components/SliderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-carousel cycle hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item contain v-for="(slide ,i) in slides" :key="i" eager>
+        <v-img contain height="100%" :src="slide.image.url" @click="redirect(slide.url)" eager/> </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import Slider from "@/models/Slider";
+import SliderService from "@/services/SliderService";
+
+@Component
+export default class SliderComponent extends Vue {
+  slides: Slider[] = []
+
+
+  created() {
+    SliderService.getSliders(this, this.slides)
+  }
+
+  redirect(url : string) {
+    window.open(url, "_blank")
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <SliderComponent/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import SliderComponent from "@/components/SliderComponent.vue";
 
-@Component
+@Component({components: {SliderComponent}})
 export default class Home extends Vue {}
 </script>
