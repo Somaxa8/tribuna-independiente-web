2e015ad7379d31f6f2c7cfdcfd7b3d806a2df142
Somacode <silviofrancoxa8@gmail.com>
2021-06-25 17:33:58 -0500

    fixed auditing

--- src/models/Auditing.ts
+++ src/models/Auditing.ts
@@ -1,16 +1,37 @@
-import {JsonProperty} from "json2typescript";
+import {JsonObject, JsonProperty} from "json2typescript";
 import DateTimeConverter from "@/services/converters/DateTimeConverter";
 import {DateTime} from "luxon";
+import User from "@/models/User";
 
+@JsonObject("Auditing")
 export default class Auditing {
 
     @JsonProperty("createdAt", DateTimeConverter, true)
-    createdAt?: DateTime = undefined
+    private _createdAt?: DateTime = undefined
 
     @JsonProperty("updatedAt", DateTimeConverter, true)
-    updatedAt?: DateTime = undefined
+    private _updatedAt?: DateTime = undefined
 
     @JsonProperty("createdBy", User, true)
-    createdBy?: User = undefined
+    private _createdBy?: User = undefined
+
+    get createdBy(): User | undefined {
+        return this._createdBy;
+    }
+    set createdBy(value: User | undefined) {
+        this._createdBy = value;
+    }
+    get updatedAt(): DateTime | undefined {
+        return this._updatedAt;
+    }
+    set updatedAt(value: DateTime | undefined ) {
+        this._updatedAt = value;
+    }
+    get createdAt(): DateTime | undefined {
+        return this._createdAt;
+    }
+    set createdAt(value: DateTime | undefined ) {
+        this._createdAt = value;
+    }
 
 }
